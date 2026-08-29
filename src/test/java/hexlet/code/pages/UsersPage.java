df11dcdaae8016037b779353656f2321b39b5fbc
Constantine <foofightermail@gmail.com>
2026-08-29 14:09:13 +0300

    updated isEmptyStateDisplayed

--- src/test/java/hexlet/code/pages/UsersPage.java
+++ src/test/java/hexlet/code/pages/UsersPage.java
@@ -79,266 +79,276 @@ public final class UsersPage extends BasePage {
         }
     }
 
-public boolean isUserTableLoaded() {
-    try {
-        getWait().until(ExpectedConditions.visibilityOfElementLocated(userListContainer));
+    public boolean isUserTableLoaded() {
+        try {
+            getWait().until(ExpectedConditions.visibilityOfElementLocated(userListContainer));
 
-        getWait().until(ExpectedConditions.presenceOfElementLocated(userRow));
-        return true;
-    } catch (Exception e) {
-        return false;
-    }
-}
-
-public boolean areKeyFieldsDisplayed() {
-    try {
-        getWait().until(ExpectedConditions.visibilityOfElementLocated(userListContainer));
-
-        String theadText = getDriver().findElement(By.tagName("thead")).getText();
-
-        boolean hasId = theadText.contains("Id");
-        boolean hasEmail = theadText.contains("Email");
-        boolean hasFirstName = theadText.contains("First name");
-        boolean hasLastName = theadText.contains("Last name");
-        boolean hasCreatedAt = theadText.contains("Created at");
-
-        return hasId && hasEmail && hasFirstName && hasLastName && hasCreatedAt;
-    } catch (Exception e) {
-        return false;
-    }
-}
-
-public void clickEditUser(String expectedName) {
-    By userRowToEdit = By.xpath("//*[contains(text(), '" + expectedName + "')]");
-
-    getWait().until(ExpectedConditions.elementToBeClickable(userRowToEdit)).click();
-}
-
-public boolean isEmailValidationErrorDisplayed() {
-    try {
-        getWait().until(ExpectedConditions.presenceOfElementLocated(emailErrorMessage));
-        return true;
-    } catch (Exception e) {
-        return false;
-    }
-}
-
-public String getFirstNameValue() {
-    return getWait().until(ExpectedConditions.
-            visibilityOfElementLocated(firstNameField)).getAttribute("value");
-}
-
-public String getEmailValue() {
-    return getWait().until(ExpectedConditions.visibilityOfElementLocated(emailField)).getAttribute("value");
-}
-
-public void fillEmailOnly(String email) {
-    clearAndType(emailField, email);
-}
-
-public void clickSaveButtonForUsers() {
-    getWait().until(ExpectedConditions.elementToBeClickable(saveButton)).click();
-}
-
-public void clickExportButtonForUsers() {
-    getWait().until(ExpectedConditions.elementToBeClickable(exportButton)).click();
-}
-
-public void forceGoToUsers() {
-    By usersIcon = By.xpath("(//*[@data-testid='ViewListIcon'])[2]");
-
-    getWait().until(ExpectedConditions.elementToBeClickable(usersIcon)).click();
-}
-
-private void clearAndType(By fieldLocator, String text) {
-    WebElement input = getWait().until(ExpectedConditions.elementToBeClickable(fieldLocator));
-
-    input.click();
-
-
-    new Actions(getDriver())
-            .keyDown(Keys.CONTROL).sendKeys("a").keyUp(Keys.CONTROL)
-            .sendKeys(Keys.BACK_SPACE)
-            .sendKeys(text)
-            .perform();
-}
-
-public void clickDeleteButton() {
-    getWait().until(ExpectedConditions.elementToBeClickable(deleteButton)).click();
-}
-
-public void clickDeleteAllUsersButton() {
-    getWait().until(ExpectedConditions.elementToBeClickable(deleteAllUsersButton)).click();
-}
-
-public void clickSelectAllUsersButton() {
-    WebElement checkbox = getWait().until(ExpectedConditions.presenceOfElementLocated(selectAllCheckbox));
-    JavascriptExecutor js = (JavascriptExecutor) getDriver();
-    js.executeScript("arguments[0].click();", checkbox);
-}
-
-public boolean isEmptyStateDisplayed() {
-
-    long startTime = System.currentTimeMillis();
-
-    while (System.currentTimeMillis() - startTime < MAX_WAIT_DURATION) {
-        String pageSource = getDriver().getPageSource();
-
-        if (pageSource.contains("No Users yet.") && pageSource.contains("Do you want to add one?")) {
+            getWait().until(ExpectedConditions.presenceOfElementLocated(userRow));
             return true;
+        } catch (Exception e) {
+            return false;
         }
     }
-    return false;
-}
 
-public void clickUpperShowButton() {
-    By showButtonLocator = By.xpath("//a[contains(@href, '/show')]");
-    WebElement showButton = getWait().until(ExpectedConditions.elementToBeClickable(showButtonLocator));
-    showButton.click();
-}
+    public boolean areKeyFieldsDisplayed() {
+        try {
+            getWait().until(ExpectedConditions.visibilityOfElementLocated(userListContainer));
 
-public void clickUpperEditButton() {
-    By editButtonLocator =
-            By.xpath("//a[contains(@class, 'MuiButton-root') and (contains(text(), 'Edit'))]");
-    WebElement showButton = getWait().until(ExpectedConditions.elementToBeClickable(editButtonLocator));
-    showButton.click();
-}
+            String theadText = getDriver().findElement(By.tagName("thead")).getText();
 
-public boolean isTextPresentOnViewPage(String expectedText) {
-    By textLocator = By.xpath("//span[contains(@class, 'MuiTypography-body2') and text()='"
-            +
-            expectedText + "']");
-    try {
-        WebElement element =
-                getWait().until(ExpectedConditions.visibilityOfElementLocated(textLocator));
-        return element.isDisplayed();
-    } catch (Exception e) {
-        return false;
+            boolean hasId = theadText.contains("Id");
+            boolean hasEmail = theadText.contains("Email");
+            boolean hasFirstName = theadText.contains("First name");
+            boolean hasLastName = theadText.contains("Last name");
+            boolean hasCreatedAt = theadText.contains("Created at");
+
+            return hasId && hasEmail && hasFirstName && hasLastName && hasCreatedAt;
+        } catch (Exception e) {
+            return false;
+        }
     }
-}
 
-public void clearFirstNameField() {
-    By locator = By.name("firstName");
-    WebElement input = getWait().until(ExpectedConditions.elementToBeClickable(locator));
-    input.click();
-    input.sendKeys(Keys.END);
-    input.sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME));
-    input.sendKeys(Keys.BACK_SPACE);
-}
+    public void clickEditUser(String expectedName) {
+        By userRowToEdit = By.xpath("//*[contains(text(), '" + expectedName + "')]");
 
-public void clearLastNameField() {
-    By locator = By.name("lastName");
-    WebElement input = getWait().until(ExpectedConditions.elementToBeClickable(locator));
-    input.click();
-    input.sendKeys(Keys.END);
-    input.sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME));
-    input.sendKeys(Keys.BACK_SPACE);
-}
-
-public void clearEmailField() {
-    By locator = By.name("email");
-    WebElement input = getWait().until(ExpectedConditions.elementToBeClickable(locator));
-    input.click();
-    input.sendKeys(Keys.END);
-    input.sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME));
-    input.sendKeys(Keys.BACK_SPACE);
-}
-
-public boolean isInvalidEmailErrorDisplayed() {
-    By errorLocator = By.xpath("//*[contains(text(), 'Incorrect email format')]");
-
-    try {
-        WebElement errorMessage = getWait().until(ExpectedConditions.visibilityOfElementLocated(errorLocator));
-        return errorMessage.isDisplayed();
-    } catch (Exception e) {
-        return false;
+        getWait().until(ExpectedConditions.elementToBeClickable(userRowToEdit)).click();
     }
-}
 
-public void fillFirstNameField(String firstName) {
-    By nameLocator = By.name("firstName");
-    WebElement nameInput = getWait().until(ExpectedConditions.elementToBeClickable(nameLocator));
-    nameInput.sendKeys(firstName);
-}
+    public boolean isEmailValidationErrorDisplayed() {
+        try {
+            getWait().until(ExpectedConditions.presenceOfElementLocated(emailErrorMessage));
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
 
-public void fillLastNameField(String lastName) {
-    By nameLocator = By.name("lastName");
-    WebElement nameInput = getWait().until(ExpectedConditions.elementToBeClickable(nameLocator));
-    nameInput.sendKeys(lastName);
-}
+    public String getFirstNameValue() {
+        return getWait().until(ExpectedConditions.
+                visibilityOfElementLocated(firstNameField)).getAttribute("value");
+    }
 
-public void fillEmailField(String email) {
-    By nameLocator = By.name("email");
-    WebElement nameInput = getWait().until(ExpectedConditions.elementToBeClickable(nameLocator));
-    nameInput.sendKeys(email);
-}
+    public String getEmailValue() {
+        return getWait().until(ExpectedConditions.visibilityOfElementLocated(emailField)).getAttribute("value");
+    }
 
-public String getFirstNameInputValue() {
-    By firstNameInputLocator = By.cssSelector("input[name='firstName']");
-    WebElement editFirstNameInput = getWait().until(ExpectedConditions.
-            visibilityOfElementLocated(firstNameInputLocator));
-    return editFirstNameInput.getAttribute("value");
-}
+    public void fillEmailOnly(String email) {
+        clearAndType(emailField, email);
+    }
 
-public String getLastNameInputValue() {
-    By firstNameInputLocator = By.cssSelector("input[name='lastName']");
-    WebElement editLastNameInput =
-            getWait().until(ExpectedConditions.visibilityOfElementLocated(firstNameInputLocator));
-    return editLastNameInput.getAttribute("value");
-}
+    public void clickSaveButtonForUsers() {
+        getWait().until(ExpectedConditions.elementToBeClickable(saveButton)).click();
+    }
 
-public String getEmailInputValue() {
-    By emailInputLocator = By.cssSelector("input[name='email']");
-    WebElement editFirstNameInput =
-            getWait().until(ExpectedConditions.visibilityOfElementLocated(emailInputLocator));
-    return editFirstNameInput.getAttribute("value");
-}
+    public void clickExportButtonForUsers() {
+        getWait().until(ExpectedConditions.elementToBeClickable(exportButton)).click();
+    }
 
-public int getUsersCount() {
-    return getDriver().findElements(By.cssSelector(".MuiTableRow-root")).size();
-}
+    public void forceGoToUsers() {
+        By usersIcon = By.xpath("(//*[@data-testid='ViewListIcon'])[2]");
 
-public int getFinalUsersCount() {
-    return getDriver().findElements(By.cssSelector(".MuiTableRow-root")).size();
-}
+        getWait().until(ExpectedConditions.elementToBeClickable(usersIcon)).click();
+    }
 
-public void waitForSnackBar() {
-    getWait().until(ExpectedConditions.
-            visibilityOfElementLocated(By.cssSelector(".MuiAlert-root, .MuiSnackbar-root")));
-}
+    private void clearAndType(By fieldLocator, String text) {
+        WebElement input = getWait().until(ExpectedConditions.elementToBeClickable(fieldLocator));
 
-public void waitForPaginationTextOneToFive() {
-    By paginationTextLocator = By.xpath("//p[contains(@class, 'MuiTablePagination-displayedRows')]");
-    getWait().until(ExpectedConditions.textToBePresentInElementLocated(paginationTextLocator, "1-5"));
-}
+        input.click();
 
-public void waitForPaginationTextSixToMore() {
-    By paginationTextLocator = By.xpath("//p[contains(@class, 'MuiTablePagination-displayedRows')]");
-    getWait().until(ExpectedConditions.textToBePresentInElementLocated(paginationTextLocator, "6-"));
-}
 
-public String getCurrentUrl() {
-    return getDriver().getCurrentUrl();
-}
+        new Actions(getDriver())
+                .keyDown(Keys.CONTROL).sendKeys("a").keyUp(Keys.CONTROL)
+                .sendKeys(Keys.BACK_SPACE)
+                .sendKeys(text)
+                .perform();
+    }
 
-public boolean isTextPresentOnPage(String text) {
-    return getDriver().getPageSource().contains(text);
-}
+    public void clickDeleteButton() {
+        getWait().until(ExpectedConditions.elementToBeClickable(deleteButton)).click();
+    }
 
-public void clickEmailColumnHeader() {
-    WebElement emailHeader = getDriver().findElement(By.cssSelector("[data-field='email']"));
-    emailHeader.click();
-}
+    public void clickDeleteAllUsersButton() {
+        getWait().until(ExpectedConditions.elementToBeClickable(deleteAllUsersButton)).click();
+    }
 
-public String getEmailFromFirstRow() {
-    WebElement firstRowEmailCell = getDriver().findElement(By.cssSelector("tbody .column-email"));
-    return firstRowEmailCell.getText().trim();
-}
+    public void clickSelectAllUsersButton() {
+        WebElement checkbox = getWait().until(ExpectedConditions.presenceOfElementLocated(selectAllCheckbox));
+        JavascriptExecutor js = (JavascriptExecutor) getDriver();
+        js.executeScript("arguments[0].click();", checkbox);
+    }
 
-public boolean isSaveButtonEnabled() {
-    WebElement saveBtn = getDriver().findElement(By.xpath("//*[contains(text(), 'Save')]"));
+    public boolean isEmptyStateDisplayed() {
+        try {
+            getWait().until(ExpectedConditions.visibilityOfElementLocated(By.xpath
+                    ("//*[contains(text(), 'No Users yet')]")));
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
 
-    return saveBtn.isEnabled();
-}
+//    long startTime = System.currentTimeMillis();
+//
+//    while (System.currentTimeMillis() - startTime < MAX_WAIT_DURATION) {
+//        String pageSource = getDriver().getPageSource();
+//
+//        if (pageSource.contains("No Users yet.") && pageSource.contains("Do you want to add one?")) {
+//            return true;
+//        }
+//    }
+//    return false;
+    }
+
+    public void clickUpperShowButton() {
+        By showButtonLocator = By.xpath("//a[contains(@href, '/show')]");
+        WebElement showButton = getWait().until(ExpectedConditions.elementToBeClickable(showButtonLocator));
+        showButton.click();
+    }
+
+    public void clickUpperEditButton() {
+        By editButtonLocator =
+                By.xpath("//a[contains(@class, 'MuiButton-root') and (contains(text(), 'Edit'))]");
+        WebElement showButton = getWait().until(ExpectedConditions.elementToBeClickable(editButtonLocator));
+        showButton.click();
+    }
+
+    public boolean isTextPresentOnViewPage(String expectedText) {
+        By textLocator = By.xpath("//span[contains(@class, 'MuiTypography-body2') and text()='"
+                +
+                expectedText + "']");
+        try {
+            WebElement element =
+                    getWait().until(ExpectedConditions.visibilityOfElementLocated(textLocator));
+            return element.isDisplayed();
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public void clearFirstNameField() {
+        By locator = By.name("firstName");
+        WebElement input = getWait().until(ExpectedConditions.elementToBeClickable(locator));
+        input.click();
+        input.sendKeys(Keys.END);
+        input.sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME));
+        input.sendKeys(Keys.BACK_SPACE);
+    }
+
+    public void clearLastNameField() {
+        By locator = By.name("lastName");
+        WebElement input = getWait().until(ExpectedConditions.elementToBeClickable(locator));
+        input.click();
+        input.sendKeys(Keys.END);
+        input.sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME));
+        input.sendKeys(Keys.BACK_SPACE);
+    }
+
+    public void clearEmailField() {
+        By locator = By.name("email");
+        WebElement input = getWait().until(ExpectedConditions.elementToBeClickable(locator));
+        input.click();
+        input.sendKeys(Keys.END);
+        input.sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME));
+        input.sendKeys(Keys.BACK_SPACE);
+    }
+
+    public boolean isInvalidEmailErrorDisplayed() {
+        By errorLocator = By.xpath("//*[contains(text(), 'Incorrect email format')]");
+
+        try {
+            WebElement errorMessage = getWait().until(ExpectedConditions.visibilityOfElementLocated(errorLocator));
+            return errorMessage.isDisplayed();
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public void fillFirstNameField(String firstName) {
+        By nameLocator = By.name("firstName");
+        WebElement nameInput = getWait().until(ExpectedConditions.elementToBeClickable(nameLocator));
+        nameInput.sendKeys(firstName);
+    }
+
+    public void fillLastNameField(String lastName) {
+        By nameLocator = By.name("lastName");
+        WebElement nameInput = getWait().until(ExpectedConditions.elementToBeClickable(nameLocator));
+        nameInput.sendKeys(lastName);
+    }
+
+    public void fillEmailField(String email) {
+        By nameLocator = By.name("email");
+        WebElement nameInput = getWait().until(ExpectedConditions.elementToBeClickable(nameLocator));
+        nameInput.sendKeys(email);
+    }
+
+    public String getFirstNameInputValue() {
+        By firstNameInputLocator = By.cssSelector("input[name='firstName']");
+        WebElement editFirstNameInput = getWait().until(ExpectedConditions.
+                visibilityOfElementLocated(firstNameInputLocator));
+        return editFirstNameInput.getAttribute("value");
+    }
+
+    public String getLastNameInputValue() {
+        By firstNameInputLocator = By.cssSelector("input[name='lastName']");
+        WebElement editLastNameInput =
+                getWait().until(ExpectedConditions.visibilityOfElementLocated(firstNameInputLocator));
+        return editLastNameInput.getAttribute("value");
+    }
+
+    public String getEmailInputValue() {
+        By emailInputLocator = By.cssSelector("input[name='email']");
+        WebElement editFirstNameInput =
+                getWait().until(ExpectedConditions.visibilityOfElementLocated(emailInputLocator));
+        return editFirstNameInput.getAttribute("value");
+    }
+
+    public int getUsersCount() {
+        return getDriver().findElements(By.cssSelector(".MuiTableRow-root")).size();
+    }
+
+    public int getFinalUsersCount() {
+        return getDriver().findElements(By.cssSelector(".MuiTableRow-root")).size();
+    }
+
+    public void waitForSnackBar() {
+        getWait().until(ExpectedConditions.
+                visibilityOfElementLocated(By.cssSelector(".MuiAlert-root, .MuiSnackbar-root")));
+    }
+
+    public void waitForPaginationTextOneToFive() {
+        By paginationTextLocator = By.xpath("//p[contains(@class, 'MuiTablePagination-displayedRows')]");
+        getWait().until(ExpectedConditions.textToBePresentInElementLocated(paginationTextLocator, "1-5"));
+    }
+
+    public void waitForPaginationTextSixToMore() {
+        By paginationTextLocator = By.xpath("//p[contains(@class, 'MuiTablePagination-displayedRows')]");
+        getWait().until(ExpectedConditions.textToBePresentInElementLocated(paginationTextLocator, "6-"));
+    }
+
+    public String getCurrentUrl() {
+        return getDriver().getCurrentUrl();
+    }
+
+    public boolean isTextPresentOnPage(String text) {
+        List<WebElement> elements = getDriver().findElements(By.xpath
+                ("//*[contains(text(), '" + text + "')]"));
+        return elements.stream().anyMatch(WebElement::isDisplayed);
+        //return getDriver().getPageSource().contains(text);
+    }
+
+    public void clickEmailColumnHeader() {
+        WebElement emailHeader = getDriver().findElement(By.cssSelector("[data-field='email']"));
+        emailHeader.click();
+    }
+
+    public String getEmailFromFirstRow() {
+        WebElement firstRowEmailCell = getDriver().findElement(By.cssSelector("tbody .column-email"));
+        return firstRowEmailCell.getText().trim();
+    }
+
+    public boolean isSaveButtonEnabled() {
+        WebElement saveBtn = getDriver().findElement(By.xpath("//*[contains(text(), 'Save')]"));
+
+        return saveBtn.isEnabled();
+    }
 }
 
